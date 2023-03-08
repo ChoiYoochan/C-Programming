8a75c9f6ed2ec64d70bbcc04b821f17883e38c02
ChoiYoochan <125718768+ChoiYoochan@users.noreply.github.com>
2023-03-08 11:52:43 +0900

    Add files via upload

---
+++ 20230308_1/20230308-1/main.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>  //heaer 파일추가 standard IO
+
+
+int main(void)
+{
+
+	printf("test \n");  //\n l ine feed
+	printf("202315017 \n");  //\n l ine feed
+	printf("Yuc Han CHOI \n");  //\n l ine feed
+	return 0;
+}
+
+//ctrl+F5 는 실행
+
+
+
+
+
+	
+
